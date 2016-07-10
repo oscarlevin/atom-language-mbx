90e0f762956355dc51d613e56119405e4170d6e8
Oscar Levin <oscar.levin@gmail.com>
2016-07-10 14:09:04 -0600

    first commit

---
+++ spec/mbxtools-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MbxtoolsView from '../lib/mbxtools-view';
+
+describe('MbxtoolsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
